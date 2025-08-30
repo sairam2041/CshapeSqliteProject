54edf484e017a41b4e1c8aa0118f8deb6e2d824b
sairam2041 <63296834+sairam2041@users.noreply.github.com>
2025-08-30 23:30:26 +0900

    初回コミット

---
+++ WpfApp/Models/Title.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+public class Title : INotifyPropertyChanged
+{
+    private string _titleName;
+    public string TitleName
+    {
+        get => _titleName;
+        set
+        {
+            _titleName = value;
+            OnPropertyChanged(nameof(TitleName));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
