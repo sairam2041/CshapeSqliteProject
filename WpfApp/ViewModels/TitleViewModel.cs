e2199cf658a3b35bcb2b18f3f9f6eaa84bd6a27b
sairam2041 <63296834+sairam2041@users.noreply.github.com>
2025-09-07 11:49:30 +0900

    INotifyPropertyChanged確認用処理を実装

--- WpfApp/ViewModels/TitleViewModel.cs
+++ WpfApp/ViewModels/TitleViewModel.cs
@@ -8,16 +8,35 @@ namespace WpfApp.ViewModels
     internal class TitleViewModel : INotifyPropertyChanged
     {
         private TitleModel _model;
-        public string TitleName => _model.TitleName;
+        private string _titleName;
+        public string TitleName
+        {
+            get => _titleName;
+            set
+            {
+                if (_titleName != value)
+                {
+                    _titleName = value;
+                    OnPropertyChanged(nameof(TitleName)); // UIが更新される
+                }
+            }
+        }
+
         public ICommand ShowMessageCommand { get; }
 
         public TitleViewModel()
         {
             _model = new TitleModel { TitleName = "C#とSQLite(ついでにWPF)習熟プロジェクト" };
-            ShowMessageCommand = new RelayCommand(_ => MessageBox.Show("test"));
+            _titleName = _model.TitleName;
+            //ShowMessageCommand = new RelayCommand(_ => MessageBox.Show("test"));
+            ShowMessageCommand = new RelayCommand(_ =>
+            {
+                _model.TitleName = "ちゃんと変わった？";
+                TitleName = _model.TitleName;
+            });
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
         protected void OnPropertyChanged(string name) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
